b0a76265c2cf1a25a901e1514d5ae101833e2c5c
Ali bina <alibinabajii@gmail.com>
2021-12-09 23:00:48 +0330

    search section layout complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <main>
-        
+        <div className="searchForm">
+          <div className="searchForm__input">
+            <input type="text"></input>
+          </div>
+          <div className="searchForm__button">
+            <button>
+             جستجو
+            </button>
+          </div>
+        </div>
       </main>
     </div>
   );
