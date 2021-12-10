5688692f8d8968d6f336d767e3fba6c6d0646b8c
Ali bina <alibinabajii@gmail.com>
2021-12-10 14:24:56 +0330

    api get data and show on table handled

--- src/App.js
+++ src/App.js
@@ -2,29 +2,59 @@ import './App.scss';
 import React, { useState } from 'react';
 import axios from 'axios';
 function App() {
-  const getData = () => {
+  const getData = (location, x, y) => {
     const BASE_URL = process.env.REACT_APP_BASE_URL;
     const API_KEY = process.env.REACT_APP_API_KEY;
     const headers = {
       'Api-Key': API_KEY
   };
-    axios.get(BASE_URL + "term=حرم&lat=59.6157432&lng=36.2880443", { headers })
-        .then(response => setData(response))
+    axios.get(BASE_URL + "term=" + location + "&lat=" + x +"&lng=" + y, { headers })
+        .then(response => setData(response.data.items))
         .catch(error => {
           console.error('There was an error!', error);
       });
   }
-  const [data, setData] = useState();
+  const termInputHandler = (e) =>{
+    setTerm(e.target.value)
+    if (e.target.value === ""){
+      setData(null)
+    }
+  }
+  const serachHandler = () =>{
+    let location = term;
+    let x = 59.6157432;
+    let y = 36.2880443;
+    getData(location, x, y);
+  }
+  const [data, setData] = useState(null);
+  const [term, setTerm] = useState("");
   return (
     <div className="App">
       <main>
-        {console.log(data)}
-        <div className="searchForm">
-          <div className="searchForm__input">
-            <input type="text"></input>
+        <div className="searchSection">
+          <div className="searchForm">
+            <div className="searchForm__input">
+              <input type="text" onChange={termInputHandler}></input>
+            </div>
+            <div className="searchForm__button">
+              <button onClick={serachHandler}>جستجو</button>
+            </div>
           </div>
-          <div className="searchForm__button">
-            <button onClick={getData}>جستجو</button>
+          <div className={data !== null && term !=="" ?"searchResult" : "searchResult hide"}>
+          {data !== null ? data.length === 0 ? <span className="notFoundData">متاسفانه نتیجه ای یافت نشد.</span> :
+          <table className="searchResult__table">
+            <tr className="title">
+              <th>عنوان</th>
+              <th>منطقه</th>
+            </tr>
+            {data !== null ?data.map(e => (
+              <tr>
+                <td>{e.title}</td>
+                <td>{e.region}</td>
+              </tr>
+              )) : null}
+          </table>
+          : null}
           </div>
         </div>
       </main>
